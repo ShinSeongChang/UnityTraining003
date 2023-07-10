ba173f6e9c2baafe029f0aa50e819e66815414db
DESKTOP-51TGSF5\KGA <dlwnsdud0612@gmail.com>
2023-07-10 21:33:32 +0900

    Init : New Project. Traning003 - solo . set up

---
+++ 23.07.10 Training/Assets/Shin/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = default;
+
+    public bool isGameover = false;
+    public Text scoreText = default;
+    public GameObject gameoverUi = default;
+
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
